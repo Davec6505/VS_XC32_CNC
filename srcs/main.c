e872a364f13b2d84c800e5ae81abeab5e2568de9
Davec6505 <davec6505@gmail.com>
2025-07-23 07:29:06 +0100

    First commit

---
+++ srcs/main.c
@@ -0,0 +1,26 @@
+#include "xc.h" // Include the appropriate header file for your PIC32MZ
+
+
+
+// Function prototype
+void CPU_Initialize(void);
+
+int main(void)
+{
+    // Call the initialization function
+    CPU_Initialize();
+
+    // Your main application code here
+
+    while (1)
+    {
+        // Main loop
+    }
+
+    return 0;
+}
+
+void CPU_Initialize(void)
+{
+
+}
